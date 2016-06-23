86488fe29be0e4b9210e47a4fc21d95d87e24557
Pierry Borges <pieerry@gmail.com>
2016-06-23 18:26:44 -0300

    Continuacao home

--- cartolapp/app/src/main/java/com/github/pierry/cartolapp/ui/fragments/TeamFragment.java
+++ cartolapp/app/src/main/java/com/github/pierry/cartolapp/ui/fragments/TeamFragment.java
@@ -1,11 +1,18 @@
 package com.github.pierry.cartolapp.ui.fragments;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 import at.markushi.ui.CircleButton;
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
+import com.github.javiersantos.materialstyleddialogs.enums.Style;
 import com.github.pierry.cartolapp.R;
 import com.github.pierry.cartolapp.api.TeamApi;
 import com.github.pierry.cartolapp.api.contracts.ITeamApi;
@@ -14,7 +21,8 @@ import com.github.pierry.cartolapp.domain.contracts.ITeamRepository;
 import com.github.pierry.cartolapp.repositories.TeamRepository;
 import com.github.pierry.cartolapp.ui.adapters.TeamAdapter;
 import com.github.pierry.fitloader.RotateLoading;
-import java.util.ArrayList;
+import com.github.pierry.simpletoast.SimpleToast;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import java.util.List;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
@@ -42,7 +50,28 @@ import org.androidannotations.annotations.ViewById;
   }
 
   @Click void action() {
-    teamApi.get("LambisqueirosFC");
+    LayoutInflater inflater = LayoutInflater.from(getActivity());
+    View view = inflater.inflate(R.layout.add_team, null);
+    final MaterialEditText material = (MaterialEditText) view.findViewById(R.id.teamRequest);
+    MaterialStyledDialog alert = new MaterialStyledDialog(getActivity());
+    alert.setTitle(getString(R.string.add_team));
+    alert.setCustomView(view);
+    alert.setPositive(getString(R.string.add), new MaterialDialog.SingleButtonCallback() {
+      @Override public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+        String request = material.getText().toString();
+        if (request != null && !request.equals("")) {
+          teamApi.get(request);
+        } else {
+          SimpleToast.error(getActivity(), getString(R.string.error_string));
+        }
+      }
+    });
+    alert.setNegative(getString(R.string.cancel), null);
+    alert.withDarkerOverlay(true);
+    alert.setCancelable(true);
+    alert.setIcon(R.mipmap.ic_launcher);
+    alert.setStyle(Style.HEADER_WITH_TITLE);
+    alert.show();
   }
 
   @UiThread void loadRecyclerViewConfig() {
